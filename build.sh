d462d0d728b51716fa816c847b8f969353ba8ed8
Marlon Alkan <info@maluu.eu>
2024-12-31 17:54:19 +0100

    update to BIRD 2.16, remove mipsel, split configuring and compiling

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-export BIRD_VERSION=2.13
+export BIRD_VERSION=2.16
 
 rm -rf out
 mkdir out
 
 DOCKER_BUILDKIT=1 docker build -o out/bird-$BIRD_VERSION/mips mips --build-arg BIRD_VERSION=${BIRD_VERSION}
-DOCKER_BUILDKIT=1 docker build -o out/bird-$BIRD_VERSION/mipsel mipsel --build-arg BIRD_VERSION=${BIRD_VERSION}
 
 cd out && tar -czvf bird-$BIRD_VERSION.tar.gz bird-$BIRD_VERSION/ && cd ..
